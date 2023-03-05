50f4bff24ca92dca576a1ddd569046a4eb2e714c
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-03-05 14:58:22 -0300

    Test

--- src/main/java/com/alexferreira/projeto/ProjetoCrudApplication.java
+++ src/main/java/com/alexferreira/projeto/ProjetoCrudApplication.java
@@ -8,7 +8,7 @@ public class ProjetoCrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetoCrudApplication.class, args);
-        System.out.println("test");
+
 
     }
 
